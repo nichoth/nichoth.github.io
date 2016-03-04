68c9d485052a993dbfbfab1596bf7f7b789cc244
nichoth <nichoth@gmail.com>
2016-03-04 14:58:13 -0800

    using hyperstream

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,36 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
+var concat = require('concat-stream');
 var fs = require('fs');
 var through = require('through2');
 var hyperstream = require('hyperstream');
 
+function gulpHyperstream(path, mapper) {
+  return through.obj(function(file, _, next) {
+    var hs = hyperstream(mapper(file.contents));
+
+    fs.createReadStream(path)
+      .pipe(hs)
+      .pipe(concat(function gotHtml(html) {
+        file.contents = html;
+        next(null, file);
+      }))
+    ;
+  });
+
+}
+
 gulp.task('template', function() {
-  gulp.src('src/pages/*.html')
-    .pipe(through.obj(function(file, _, next) {
-
-      var hs = hyperstream({
+  gulp.src('src/content/*.html')
+    .pipe(gulpHyperstream(__dirname+'/src/layouts/index.html', (html) => {
+      return {
         '#content': {
-          _prependHtml: file.contents
+          _prependHtml: html
         }
-      });
-
-      file.contents = fs.createReadStream(__dirname+'/src/index.html')
-        .pipe(hs)
-      ;
-      next(null, file);
+      };
     }))
     .pipe(rename((path) => {
+      if (path.basename === 'index') return;
       path.dirname = path.basename;
       path.basename = 'index'
     }))
